6087c53c1ee8b9e5c0795764f838e3a262aaa06c
takeda810 <ass08nn@gmail.com>
2023-08-11 22:46:20 +0900

    top

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,9 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="/css/reset.css" >
+    <link rel="stylesheet" href="/css/style.css" >
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <title>AtelierFleur</title>
 </head>
 <body>
-    
+    <div class="MainPhoto">
+        <div class="logo">
+            <img id="logo" src="/img/logo.png">
+            <p>アトリエ・フルール</p>
+        </div>
+        <div class="top">
+            <div class="logo_fleur">
+                <img id="top_logo_fleur" src="/img/main_fleur.png">
+                <div class="top_text">
+                    <p>笑顔に変わる瞬間を</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="main">
+        <div class="main_concept">
+            <h2>concept</h2>
+            <p>コンセプト</p>
+            <div class="concept_txt">
+                <p>text</p>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
