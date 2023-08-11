4649c397158b22b5fce45ca7317aad72accd8c26
takeda810 <ass08nn@gmail.com>
2023-08-12 00:55:01 +0900

    story

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,7 +29,33 @@
             <h2>concept</h2>
             <p>コンセプト</p>
             <div class="concept_txt">
-                <p>text</p>
+                <p>　滋賀県甲賀市にあるアーティフィシャルフラワーとキャンドル&アロマワックスのアレンジメント教室です。</br>
+                　忙しい毎日にお花と香り、揺らぎ効果のあるキャンドルを日常に取り入れリラックス。</p>
+                <p id="concept_txt_main">《笑顔に変わる瞬間を》</p>
+                <p>をコンセプトに作る楽しさをご提供させて頂きます。少人数(4人まで)完全予約制のアットフォームな教室です。</p>
+                <p style="font-size: 11px;">※入会金・年会費は頂いておりません</p>
+            </div>
+        </div>
+    </div>
+    <div class="main2">
+        <div class="main_concept">
+            <h2>MyStory</h2>
+            <p>マイストーリー</p>
+            <div class="story01">
+                <div class="story1">
+                </div>
+                <div class="story2">
+                    <p id="story_txt">Creativeな世界へ!</p>
+                    <p>医療現場に20年の元看護師</br>INIS×BERRIES、クリエイトアカデミー、ジャパンライセンスアカデミー、ディプロマ取得。JLA認定講師。Cara Fioreアロマワックス認定校。Atelier Fleur、tiédeur candle主宰。</p>
+                </div>
+            </div>
+            <div class="story02">
+                <div class="story22">
+                    <p id="story_txt">sale & order</p>
+                    <p>artificial flower ＆ candleの販売、ご注文も受け賜ります。プレゼントやディスプレイのご相談もお気軽にお問い合わせ下さい。</p>
+                </div>
+                <div class="story11">
+                </div>
             </div>
         </div>
     </div>
